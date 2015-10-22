3cbad4207b8bf2d592b4a1dd4c60119295bf7d52
angelo2dot0 <angeloaramayo@gmail.com>
2015-10-22 16:51:15 -0700

    initial express server

---
+++ server.js
@@ -0,0 +1,11 @@
+//At the top of your server.js file, require the express and body-parser modules to get started.
+//Use express() to create your app object.
+//At the bottom of your server.js file, use app.listen to set up your server to listen on a port. We usually use port 3000.
+//Run node server.js, npm start, or nodemon in the Terminal to start your server.
+//If you gave app.listen a callback with a log message, you should see the log message in your Terminal.
+//Go to localhost:3000/ to see another message that lets you know your server is listening (but a bit unhappy).
+//In your server code, between the requires and the app.listen, start a section for routes!
+//Create a route to handle GET requests at the / path.
+//Use a simple res.send to send a message that the route is working.
+//Restart your server, reload the page, and you should see the message you sent as a response. Yay!
+
